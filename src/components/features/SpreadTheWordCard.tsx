41d0e2d9f50b928aa4e60c0fdcb56284fec736cd
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-07-02 16:54:33 +0000

    Error Details: CardFooter is not defined

--- src/components/features/SpreadTheWordCard.tsx
+++ src/components/features/SpreadTheWordCard.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Copy, Linkedin, Share2, Twitter } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
