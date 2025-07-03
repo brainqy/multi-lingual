acf0a9c43126789c3b4c438d6831c6a39455a06b
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-07-03 11:59:28 +0000

    next

---
+++ src/lib/data/sample-data.ts
@@ -0,0 +1,16 @@
+
+export * from './data/platform';
+export * from './data/users';
+export * from './data/jobs';
+export * from './data/resumes';
+export * from './data/interviews';
+export * from './data/community';
+export * from './data/blog';
+export * from './data/gamification';
+export * from './data/monetization';
+export * from './data/surveys';
+export * from './data/announcements';
+export * from './data/promotions';
+export * from './data/requests';
+export * from './data/activities';
+export * from './data/questions';
