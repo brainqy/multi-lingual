beef498d59f8561d4ace5929e9c69192246d3fe3
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-07-04 16:17:10 +0000

    its looking weird

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -493,7 +493,10 @@ const SidebarMenu = React.forwardRef<
   <ul
     ref={ref}
     data-sidebar="menu"
-    className={cn("flex w-full min-w-0 flex-col gap-1", className)}
+    className={cn(
+      "m-0 flex w-full min-w-0 list-none flex-col gap-1 p-0",
+      className
+    )}
     {...props}
   />
 ))
