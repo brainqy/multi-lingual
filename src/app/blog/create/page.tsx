70a09df5bef6983a62e10d2795d79cd29e7d599c
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-07-29 14:27:56 +0000

    ○ Compiling /blog/create ...  ⨯ src\app\blog\create\page.tsx You cannot

--- src/app/blog/create/page.tsx
+++ src/app/blog/create/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 // This page has been moved to /src/app/(app)/blog/create/page.tsx to resolve a routing conflict.
-// This file is now empty and can be safely deleted.
+// This file is now a placeholder and can be safely deleted in the future.
 
 export default function DeprecatedCreateBlogPage() {
   return null;
