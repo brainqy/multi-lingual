fca3f117fb4a88ee9edf63c3078dbf77b4e99963
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-08-30 10:05:02 +0000

    update all imports

--- src/components/dashboards/UserDashboard.tsx
+++ src/components/dashboards/UserDashboard.tsx
@@ -36,7 +36,7 @@ import { Skeleton } from "../ui/skeleton";
 import type { UserProfile } from "@/types";
 import { useAuth } from "@/hooks/use-auth";
 import { updateUser } from "@/lib/data-services/users";
-import AiMentorSuggestions from "../../../usr/src/app/src/components/dashboards/AiMentorSuggestions";
+import AiMentorSuggestions from "@/components/dashboards/AiMentorSuggestions";
 
 interface UserDashboardProps {
   user: UserProfile;
