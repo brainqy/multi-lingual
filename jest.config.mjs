c1cc3d1acf9851e588a3ea7014e28b701fe86b34
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-08-30 17:22:29 +0000

    r u sure

--- jest.config.mjs
+++ jest.config.mjs
@@ -12,10 +12,6 @@ const config = {
   testEnvironment: 'jsdom',
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.mjs'],
-  moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
