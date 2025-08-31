a61aea8be221dc495efc311d140fe880e8991666
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-08-31 17:55:06 +0000

    Watch Usage: Press w to show more.   console.error     Error: Uncaught [

---
+++ src/jest.setup.js
@@ -0,0 +1,26 @@
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+
+// Mock window.matchMedia for Jest/JSDOM environment, which is required by embla-carousel-react
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+// Mock IntersectionObserver for Jest/JSDOM environment, required by embla-carousel
+const mockIntersectionObserver = jest.fn();
+mockIntersectionObserver.mockReturnValue({
+  observe: () => null,
+  unobserve: () => null,
+  disconnect: () => null,
+});
+window.IntersectionObserver = mockIntersectionObserver;
