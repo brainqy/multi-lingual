2b2b9a24ed0b3a3724e7a6e23979487f55f00d21
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-08-31 16:20:03 +0000

    C:\Users\DELL\Downloads\master\multi-lingual\node_modules\lucide-react\d

--- jest.config.mjs
+++ jest.config.mjs
@@ -10,12 +10,10 @@ const createJestConfig = nextJest({
 const config = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.mjs'],
- 
   testEnvironment: 'jest-environment-jsdom',
   transformIgnorePatterns: [
-    '/node_modules/(?!(lucide-react)/)',
+    '/node_modules/(?!lucide-react)/'
   ],
-  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
