b7ad5592c38b5705eb6bbd207eedf76ea3eaa56a
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-08-31 16:06:27 +0000

    npm test
    > nextn@0.1.0 test
    > jest --watch
    No tests found related to

--- jest.config.mjs
+++ jest.config.mjs
@@ -12,10 +12,10 @@ const config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.mjs'],
  
   testEnvironment: 'jest-environment-jsdom',
-  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
   transformIgnorePatterns: [
-    "/node_modules/(?!lucide-react)/"
+    '/node_modules/(?!(lucide-react)/)',
   ],
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
