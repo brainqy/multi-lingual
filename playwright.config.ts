ef3958c34ab3399f7a481a5a439245996534d559
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-09-04 09:06:34 +0000

    http://platform.localhost:9002/ use this url

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:9002',
+    baseURL: 'http://platform.localhost:9002',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -38,7 +38,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:9002',
+    url: 'http://platform.localhost:9002',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000, // 120 seconds for the server to start
   },
