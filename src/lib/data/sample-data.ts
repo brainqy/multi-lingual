7fc39126b922415bce54454653fa2ec126daf9bd
MyGuru <32195301+myprivateguru@users.noreply.github.com>
2025-09-06 15:31:52 +0530

    redundent files removed

--- src/lib/data/sample-data.ts
+++
@@ -1,4 +0,0 @@
-// This file is now obsolete as all data is managed in the database via prisma/seed.ts.
-// It can be removed.
-
-    
--- src/lib/data/surveys.ts
+++
@@ -1,4 +0,0 @@
-// This file is now obsolete as survey data is managed in the database via prisma/seed.ts.
-// It can be removed.
-
-    
