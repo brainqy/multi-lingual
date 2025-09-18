44b644cf1d20b7ac7b0411cfa9650aa0548608f3
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-09-18 11:30:16 +0000

    ReferenceError: useMemo is not defined     at TemplateEditorPage (webpac

--- src/app/(app)/admin/template-designer/[templateId]/page.tsx
+++ src/app/(app)/admin/template-designer/[templateId]/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
