f6b7e79b4cfb4e41f6d666711509ee9b20c0a72c
MyGuru <32195301+myprivateguru@users.noreply.github.com>
2025-09-27 11:41:09 +0530

    fix

--- src/app/pdf-download/page.tsx
+++ src/app/pdf-download/page.tsx
@@ -1,11 +1,39 @@
-import { ResumesDocument } from "@/components/features/resume-builder/pdf/resume/document";
-import { PDFViewer } from "@/components/pdf";
+
+
+import { Document, Page, Text, View, StyleSheet, PDFViewer } from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#E4E4E4",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+});
+
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>PDF Render Test: Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>PDF Render Test: Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
 
 export default function PdfDownloadPage() {
-     
-    <PDFViewer className='min-h-screen w-full'>
-        <ResumesDocument />
-
-    </PDFViewer>
-    
+  return (
+    <div>
+      <h1>PDF Download Page</h1>
+      <PDFViewer style={{ width: "100%", height: "90vh" }}>
+        <MyDocument />
+      </PDFViewer>
+    </div>
+  );
 }
