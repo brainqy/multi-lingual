fa93a22e7cd7fab51b18d5c5362b60527b2ea7c8
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-09-27 16:16:19 +0000

    ⨯ ./src/components/features/resume-builder/StepFinalize.tsx Module not f

--- src/next.config.ts
+++ src/next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["@react-pdf/renderer"],
+   serverExternalPackages: ["@react-pdf/renderer"],
   images: {
     unoptimized: true,
     remotePatterns: [
