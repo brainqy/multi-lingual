18cec5441682761761be40ee3b70d3bb610a37ac
MyGuru <32195301+myprivateguru@users.noreply.github.com>
2025-09-27 22:27:19 +0530

    fixx

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-   serverExternalPackages: ["@react-pdf/renderer"],
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -21,7 +20,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+webpack: (config: {
+    resolve: { alias: { canvas: boolean; encoding: boolean } };
+  }) => {
+    // Handle PDF renderer
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
 
+    return config;
+  },
+  transpilePackages: ["@react-pdf/renderer"],
 };
 
 export default nextConfig;
